3369c7131be1f51504c1c25d6ec3d5fbc61be3de
Malithmkd <malith.kavidu.34@gmail.com>
2024-11-26 21:04:37 +0530

    Form updated
    Recieve messages

--- src.js
+++ src.js
@@ -91,3 +91,41 @@ document.querySelectorAll('section').forEach(section => {
     observer.observe(section);
 });
 
+const Contact = document.getElementById('Contact');
+const result = document.getElementById('result');
+
+Contact.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(Contact);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            Contact.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
